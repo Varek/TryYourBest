e28ada8a573a91953fb8bed5c39f320981058dab
Ash Ryan <ash@ashryanbeats.com>
2015-11-28 14:43:34 +0100

    Using jquery to get the image element when the Edit button is clicked.

--- script.js
+++ script.js
@@ -1,31 +1,27 @@
-var featherEditor = new Aviary.Feather({
-    apiKey: '<YOUR_KEY_HERE>',
-    onSave: function(imageID, newURL) {
-        var img = document.getElementById(imageID);
-        img.src = newURL;
-        featherEditor.close();
-        console.log(newURL);
-    },
-    onError: function(errorObj) {
-        console.log(errorObj.code);
-        console.log(errorObj.message);
-        console.log(errorObj.args);
-    },
-    //tools: ['crop'],
-    // cropPresets: [
-    //     ['Size 1', '200x400'],
-    //     ['Size 2', '400x600']
-    // ],
-    // theme: 'light'
-});
+$(document).ready(function() {
+    var image;
 
-function launchEditor(id, src) {
-    featherEditor.launch({
-        image: id,
-        url: src
+    var featherEditor = new Aviary.Feather({
+        apiKey: '<YOUR_KEY_HERE>',
+        onSave: function(imageID, newURL) {
+            image.src = newURL;
+            featherEditor.close();
+            console.log(newURL);
+        },
+        onError: function(errorObj) {
+            console.log(errorObj.code);
+            console.log(errorObj.message);
+            console.log(errorObj.args);
+        }
     });
-}
 
-$('#edit-image-button').click(function(){
-    alert('Oy');
+
+    $('#edit-image-button').click(function(){
+        image = $('#editable-image')[0];
+
+        featherEditor.launch({
+            image: image.id,
+            url: image.src
+        });
+    });
 });
